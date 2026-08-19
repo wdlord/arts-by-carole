746dcd4c0b980c1aae86e388ade92b3787ea4a3c
Will Lord <williamdlord@gmail.com>
2026-08-18 18:47:58 -0700

    some initial adjustments from template

--- src/data/constants.ts
+++ src/data/constants.ts
@@ -1,42 +1,41 @@
 import { makeLinks, type NavItem, type NavLink } from "../scripts/navigation";
 
 // Website name, appended to the end of site titles and used in certain alt text.
-export const siteName = "Pilot Painting";
-export const siteUrl = "https://replace-me.com";
+export const siteName = "Arts by Carole";
+export const siteUrl = "https://artsbycarole.com";
 export const formKey = "";
-export const isTemplate = siteName == "Pilot Painting"; // template check to disable certain features
+export const isTemplate = false; // template check to disable certain features
 
-// Service names
-const serviceNames = [
-  "Interior Painting",
-  "Exterior Painting",
-  "Cabinet Finishing",
-  "Commercial Painting",
-];
+// // Service names
+// const serviceNames = [
+//   "Interior Painting",
+//   "Exterior Painting",
+//   "Cabinet Finishing",
+//   "Commercial Painting",
+// ];
 
-// Constants used to reference predefined service names.
-export const SERVICE = {
-  INTERIOR: serviceNames[0],
-  EXTERIOR: serviceNames[1],
-  CABINETS: serviceNames[2],
-  COMMERCIAL: serviceNames[3],
-};
-// Generate links for services in nav
-export const serviceLinks = makeLinks(serviceNames, '/services');
+// // Constants used to reference predefined service names.
+// export const SERVICE = {
+//   INTERIOR: serviceNames[0],
+//   EXTERIOR: serviceNames[1],
+//   CABINETS: serviceNames[2],
+//   COMMERCIAL: serviceNames[3],
+// };
+// // Generate links for services in nav
+// export const serviceLinks = makeLinks(serviceNames, '/services');
 
 // The navigation links shown in the header and footer.
 export const navLinks: NavItem[] = [
   { href: "/", label: "Home" },
   { href: "/about", label: "About" },
   {
-    label: "Services",
-    href: "/services",
+    label: "Gallery",
+    href: "/gallery",
     children: [
-      { href: "/services", label: "All Services" },
-      ...serviceLinks,
+      // ...serviceLinks,
     ],
   },
-  { href: "/gallery", label: "Gallery" },
+  { href: "/exhibits-and-recognition", label: "Exhibits and Recognition" },
   { href: "/contact", label: "Contact" },
 ];
 
@@ -50,24 +49,5 @@ export const ctaLink = "/contact";
 export var ctaTarget = "_self"; // usually doesn't need to be changed
 
 // Contact details
-export const contactEmail = "example.business@email.com";
-export const contactPhone = "(555) 123-4567";
-export const contactAddress = "Example Address";
-export const businessHours = "Mon—Fri, 9:00am—5:00pm";
-
-// Locations Served
-const state1 = "CA";
-export const locationsServed = [
-  `Escondido, ${state1}`,
-  `Oceanside, ${state1}`,
-  `El Cajon, ${state1}`,
-  `San Marcos, ${state1}`,
-  `Carlsbad, ${state1}`,
-  `Del Mar, ${state1}`,
-  `Vista, ${state1}`,
-  `Poway, ${state1}`,
-  `Encinitas, ${state1}`,
-].toSorted();
-
-export const mainLocation = "San Diego";
-export const yoe = "35";
+export const contactEmail = "carolevq@aol.com";
+export const contactPhone = "(801) 721-9242";
